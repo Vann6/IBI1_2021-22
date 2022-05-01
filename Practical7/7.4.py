ae22cb59fe03942b4ea65eaecadbebd5092555a5
Vann6 <zexiang.21@intl.zju.edu.cn>
2022-05-01 15:35:03 +0800

    Complete 7.3 and 7.4. 2022/5/1

--- Practical7/7.4.py
+++ Practical7/7.4.py
@@ -10,10 +10,8 @@ from pandas.io.parsers import TextFileReader
 os.chdir('C:/cygwin/home/13016/IBI1_2021-22/Practical7')  # change the working directory.
 covid_data = pd.read_csv("full_data(2).csv")  # import the actual data.
 dataframe1 = covid_data.head(5)
-print(dataframe1)
 # create a new dataframe including the first five rows of the dataframe of covid data.
 dataframe2 = covid_data.head(10)
-print(dataframe2)
 # create a new dataframe including the first ten rows of the dataframe of covid data.
 
 covid_data.info()
@@ -31,21 +29,17 @@ covid_data.describe()
 # the minimum of new cases is -9. Why the number of new cases can be smaller than 0?
 
 var1 = covid_data.iloc[0, 1]
-print(var1)
 # the output is 'afghanistan', in the first row and second column.
 var2 = covid_data.iloc[2, 0:5]
-print(var2)
 # output the first row and the first five columns.
 
 var3 = covid_data.iloc[0:2, :]
-print(var3)
 # output the first two rows the entire columns.
 var4 = covid_data.iloc[0:10:2, 0:5]
-print(var4)
 # output the first ten row with an interval value two and the first five columns.
-var5 = covid_data.iloc[0:3, [0, 1, 3]]
+var5 = covid_data.iloc[10:21, [0, 2]]
 print(var5)
-# output the first three rows and the first, the second, and the fourth columns.
+# output the 10-20 rows and first and third columns.
 
 my_columns = [True, True, False, True, False, False]
 var6 = covid_data.iloc[0:3, my_columns]
@@ -58,10 +52,12 @@ var6 = covid_data.iloc[0:3, my_columns]
 # var = covid_data.iloc[0:3, my_columns]
 
 var7 = covid_data.loc[2:4, 'date']
-print(var7)
 # show the data in the from 2 to 4 rows and column for date.
 
-dataframe_Afghanistan = covid_data.loc[covid_data['location'] == 'Afghanistan', "total_cases"]
+my_columns = [False, False, False, False, True, False]
+dataframe_Afghanistan = covid_data.loc[covid_data['location'] == 'Afghanistan', my_columns]
 print(dataframe_Afghanistan)
-# output a dataframe for Afghanistan.
-# we can also get the dataframe for Afghanistan by type dataframe_Afghanistan = covid_data.loc[0:81, 'total_cases']
+# use a Boolean to show "total_cases" for all rows corresponding to Afghanistan.
+# we can also get the same dataframe by type dataframe_Afghanistan = covid_data.loc[0:81, 'total_cases']
+
+
