3397bac3d2a3b9fd8baeaaae8d0f4089d8e9148d
Vann6 <zexiang.21@intl.zju.edu.cn>
2022-05-22 23:43:34 +0800

    Revise the codes.2022/5/22

--- Practical14/Practical14.py
+++ Practical14/Practical14.py
@@ -13,8 +13,6 @@ terms = collection.getElementsByTagName("term")
 # read the go_obo.xml file and get a list of "term" elements
 list_child_node_number = []
 list_child_node_number_translation = []
-list1 = []
-list_all_terms = []
 num_terms = 0
 dict = {}
 # create a dictionary that store the first childNodes and a list includes the second childNodes for each term
@@ -28,7 +26,6 @@ for term in terms:
     for i in range(is_a.length):
         is_a = term.getElementsByTagName("is_a")[i]
         is_a_txt = is_a.childNodes[0].data  # get the content of is_a
-        list1.append(is_a_txt)  # add terms that have childNodes into the list1
         if is_a_txt not in dict:
             dict[is_a_txt] = []
         dict[is_a_txt].append(term_id_txt)
@@ -37,45 +34,32 @@ for term in terms:
 print("the number of terms recorded in the Gene Ontology is "+str(num_terms))
 # the number of terms recorded in the Gene Ontology is 47340
 
+# define a function to get all the childNodes
+def unfold(list):
+    for i in range(len(list)):
+        if list[i] is not [] and type(list[i]) is list:
+            unfold(list[i])
+        else:
+            list1.append(list[i])
+
 # get all the childNodes for each term in the dictionary
 for term in terms:
     term_id = term.getElementsByTagName("id")[0]
     term_id_txt = term_id.childNodes[0].data  # get the term_id
-    A = 0
-    while A != 0:
-        list1 = dict[term_id_txt]
-        for i in range(len(list1)):
-            if type(list1[i]) is list and not []:
-                A += 1
-                break
-    # test while all childNodes are included.
-        list2 = []
-        for i in range(len(list1)):
-            if type(list1[i]) is str:
-                list2.append(list1[i])
-            else:
-                list2.extend(list1[i])
-        dict[term_id_txt] = list2
-
-
-
-
-for term in terms:
-    term_id = term.getElementsByTagName("id")[0]
-    term_id_txt = term_id.childNodes[0].data  # get the term_id
-    list1 = dict[term_id_txt]
+    list1 = []
+    unfold(dict[term_id_txt])
     list2 = []
     for i in range(len(list1)):
         if list1[i] not in list2 and list1[i] != []:
-            list2.append(list1[i])  # remove the empty list in the dictionary
-    list_child_node_number.append(len(list2))  # add the number of childNodes into the list
+            list2.append(list1[i])  # remove the empty list and repeated elements in the dictionary
     dict[term_id_txt] = list2
+    list_child_node_number.append(len(list2))  # add the number of childNodes into the list
     defstr = term.getElementsByTagName("defstr")[0]
     defstr_txt = defstr.childNodes[0].data  # get the content of defstr
     if defstr_txt.find("translation") > 0:  # test whether the term is related to translation
-        list_child_node_number_translation.append(len(list2))   # add the number of childNodes for the term that contains translation into the list
+        list_child_node_number_translation.append(len(list2))  # add the number of childNodes for the term that contains translation into the list
 
-#create a boxplot for the number of childnodes for each term
+# create a boxplot for the number of childnodes for each term
 plt.boxplot(x=[list_child_node_number,list_child_node_number_translation],
             meanline=True, showmeans=True,
             patch_artist=True, showbox=True,
