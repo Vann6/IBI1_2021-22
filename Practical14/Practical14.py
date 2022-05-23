b9c53c2f12c4b5c4cf1dd796e85c760b702beb8d
Vann6 <zexiang.21@intl.zju.edu.cn>
2022-05-23 23:31:04 +0800

    Simplify the Practical14.2022/5/23

--- Practical14/Practical14.py
+++ Practical14/Practical14.py
@@ -1,9 +1,7 @@
-
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import matplotlib.pyplot as plt
 import numpy as np
-
 
 GOtree = xml.dom.minidom.parse("go_obo.xml")
 # parse the go_obo.xml file into a DOM document
@@ -13,14 +11,20 @@ terms = collection.getElementsByTagName("term")
 # read the go_obo.xml file and get a list of "term" elements
 list_child_node_number = []
 list_child_node_number_translation = []
+list_term_translation = []
 num_terms = 0
 dict = {}
+dict_copy = {}
 # create a dictionary that store the first childNodes and a list includes the second childNodes for each term
 for term in terms:
     num_terms += 1
     is_a = term.getElementsByTagName("is_a")
     term_id = term.getElementsByTagName("id")[0]
     term_id_txt = term_id.childNodes[0].data  # get the term_id
+    defstr = term.getElementsByTagName("defstr")[0]
+    defstr_txt = defstr.childNodes[0].data.upper()  # get the content of defstr
+    if defstr_txt.count("TRANSLATION") > 0:  # test whether the term is related to translation
+        list_term_translation.append(term_id_txt)
     if term_id_txt not in dict:
         dict[term_id_txt] = []
     for i in range(is_a.length):
@@ -36,28 +40,21 @@ print("the number of terms recorded in the Gene Ontology is "+str(num_terms))
 
 # define a function to get all the childNodes
 def unfold(list):
-    for i in range(len(list)):
-        if list[i] is not [] and type(list[i]) is list:
-            unfold(list[i])
+    for n in list:
+        if type(n) == str:
+            list1.append(n)
         else:
-            list1.append(list[i])
+            unfold(n)
 
 # get all the childNodes for each term in the dictionary
-for term in terms:
-    term_id = term.getElementsByTagName("id")[0]
-    term_id_txt = term_id.childNodes[0].data  # get the term_id
+for key,value in dict.items():
     list1 = []
-    unfold(dict[term_id_txt])
-    list2 = []
-    for i in range(len(list1)):
-        if list1[i] not in list2 and list1[i] != []:
-            list2.append(list1[i])  # remove the empty list and repeated elements in the dictionary
-    dict[term_id_txt] = list2
-    list_child_node_number.append(len(list2))  # add the number of childNodes into the list
-    defstr = term.getElementsByTagName("defstr")[0]
-    defstr_txt = defstr.childNodes[0].data  # get the content of defstr
-    if defstr_txt.find("translation") > 0:  # test whether the term is related to translation
-        list_child_node_number_translation.append(len(list2))  # add the number of childNodes for the term that contains translation into the list
+    unfold(value) # add the number of childNodes into the dict
+    a = list(set(list1)) # remove the empty list and repeated elements in the dictionary
+    dict[key] = a
+    list_child_node_number.append(len(a))
+    if key in list_term_translation:
+        list_child_node_number_translation.append(len(a))  # add the number of childNodes for the term that contains translation into the list
 
 # create a boxplot for the number of childnodes for each term
 plt.boxplot(x=[list_child_node_number,list_child_node_number_translation],
@@ -82,6 +79,5 @@ else:
 
 print(var1)
 print(var2)
-
 # the number of terms recorded in the Gene Ontology is 47340
 # the term contains translation have a greater number of childNodes on average
